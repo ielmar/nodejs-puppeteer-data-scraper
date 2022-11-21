5a0a8f234b1114e22d12a4d455fff8d03a8c51a0
Elmar Ismayilov <bamazy@gmail.com>
2022-11-21 20:33:22 +0800

    sentry added

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const puppeteer = require("puppeteer-core");
 const fs = require("fs");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing")
 
 const app = express();
+
+Sentry.init({
+  dsn: "https://66ac75a682b84090b75558048ae8bc7e@o1072303.ingest.sentry.io/4504197034934272",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 const port = process.env.PORT || 3000;
 const iPhone = puppeteer.KnownDevices["iPhone 8"];
 
@@ -111,12 +136,22 @@ app.get("/start", async (req, res) => {
     res.send("Scraping Done");
   } catch (err) {
     console.log(err);
-    await browser.close();
     console.log("Browser Closed");
     res.json({
       message: "Some error occurred",
     });
   }
+});
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
 });
 
 app.listen(port, () =>
