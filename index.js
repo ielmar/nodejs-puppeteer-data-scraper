fb6fffe8cab57ad6c2b008d4ecb0c20659ef0ee6
Elmar Ismayilov <bamazy@gmail.com>
2022-11-25 22:06:20 +0800

    deleted sentry comments

--- index.js
+++ index.js
@@ -9,23 +9,13 @@ const app = express();
 Sentry.init({
   dsn: "https://66ac75a682b84090b75558048ae8bc7e@o1072303.ingest.sentry.io/4504197034934272",
   integrations: [
-    // enable HTTP calls tracing
     new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
     new Tracing.Integrations.Express({ app }),
   ],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
 
-// RequestHandler creates a separate execution context using domains, so that every
-// transaction/span/breadcrumb is attached to its own Hub instance
 app.use(Sentry.Handlers.requestHandler());
-
-// TracingHandler creates a trace for every incoming request
 app.use(Sentry.Handlers.tracingHandler());
 
 const port = process.env.PORT || 3000;
